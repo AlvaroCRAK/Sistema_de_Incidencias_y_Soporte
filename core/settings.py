227f0f845d39fccb6dd19ee60af60227ab5d941f
davguzm16 <guzman.davi16@gmail.com>
2024-10-03 14:36:09 +0100

    Creacion de las apps y botones conectores

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    "core",
+    "usuario",
+    "soporte",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,11 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "core/templates"),
+            os.path.join(BASE_DIR, "soporte/templates"),
+            os.path.join(BASE_DIR, "usuario/templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import CoreView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", CoreView.as_view() ),
+    path("usuario/", include("usuario.urls", namespace="usuario") ),
+    path("soporte/", include("soporte.urls", namespace="soporte") )
 ]
