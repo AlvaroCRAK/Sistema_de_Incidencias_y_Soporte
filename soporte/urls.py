9cc52ef3fa4b59f35865b6058b79ab5f0e5a7b1c
AlvaroECP <alvaroecp777@gmail.com>
2024-10-13 09:47:09 -0500

    historial de Incidencias en soporte implementada, falta detalles

--- soporte/urls.py
+++ soporte/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginView, IncidenciasView, IncidenciaCreateView
+from .views import LoginView, IncidenciasView, IncidenciaCreateView, IncidenciaListView, PreguntasView, IncidenciaDetailView
 
 app_name = "soporte"
 
@@ -8,4 +8,10 @@ urlpatterns = [
     path("incidencias/", IncidenciasView.as_view(), name="incidencias"), 
 
     path ( 'api/incidencias/', IncidenciaCreateView.as_view(), name = 'incidencia-create' ),
+    path('listar-incidencias/', IncidenciaListView.as_view(), name='listar_incidencias'),
+    path("preguntas/", PreguntasView.as_view(), name="preguntas"),
+
+    path('incidencias/<int:pk>/', IncidenciaDetailView.as_view(), name='detalle_incidencia'),
+
+    
 ]
--- soporte/views.py
+++ soporte/views.py
@@ -4,8 +4,54 @@ from .serializers import IncidenciaSerializer
 from django.shortcuts import render
 from django.views import View
 from rest_framework.permissions import AllowAny
+from django.views.generic import ListView
+from .models import Incidencia
+from django.views.generic import DetailView 
+from django.http import JsonResponse
 
 # Create your views here.
+
+class IncidenciaListView(ListView):
+    model = Incidencia
+    template_name = 'soporte_listar_incidencias.html'  # Plantilla que se va a renderizar
+    context_object_name = 'incidencias'  # Nombre del contexto para las incidencias
+
+    def get_context_data(self, **kwargs):
+        # Llama al método get_context_data de la clase base
+        context = super().get_context_data(**kwargs)
+
+        # Añadir variables adicionales al contexto
+        context['total_incidencias'] = Incidencia.objects.count()
+        context['incidencias_atendidas'] = Incidencia.objects.filter(estado_incidencia='Atendida').count()
+        context['incidencias_en_atencion'] = Incidencia.objects.filter(estado_incidencia='En Atención').count()
+        context['incidencias_pendientes'] = Incidencia.objects.filter(estado_incidencia='Pendiente').count()
+
+        return context  
+
+class IncidenciaDetailView(DetailView):
+    model = Incidencia
+    template_name = 'detalle_incidencia.html'  # Esto puede no ser necesario para el JSON
+
+    def get(self, request, *args, **kwargs):
+        # Llamar a la implementación base
+        self.object = self.get_object()
+        
+        # Generar un contexto adicional
+        context = self.get_context_data(object=self.object)
+        
+        # Retornar JSON si se solicita
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return JsonResponse({
+                'hora': self.object.fecha.strftime("%Y-%m-%d %H:%M"),
+                'emisor': context['emisor'],
+                'aula': context['aula'],
+                'categoria': context['categoria'],
+                'descripcion': context['descripcion'],
+                'asignado': context['asignado'],
+            })
+        else:
+            return super().get(request, *args, **kwargs)
+
 class LoginView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "soporte_login.html")
@@ -19,3 +65,8 @@ class IncidenciaCreateView ( generics.CreateAPIView ):
     queryset = Incidencia.objects.all ()
     serializer_class = IncidenciaSerializer 
     permission_classes = [AllowAny]
+
+class PreguntasView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "soporte_preguntas.html")
+
